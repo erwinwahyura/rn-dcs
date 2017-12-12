faeecc0cf6096dab63acd8c3799113ed41003b82
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-12-12 19:23:22 +0700

    remove header

--- dss/src/App.js
+++ dss/src/App.js
@@ -4,55 +4,35 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 import Login from './components/Login';
 import Home from './components/Home';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const RootNavigator = StackNavigator({
+  Login: {
+    screen: Login,
+  },
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      headerTitle: 'Home',
+    },
+  },
+}, {
+  headerMode: 'screen' 
 });
 
-const HomeScreen = StackNavigator({
-  Login: { screen: Login },
-  // Home: { screen: Home },
-});
+// export default RootNavigator;
 
 
 export default class App extends Component<{}> {
   render() {
     return (
-      // <Provider store={store}>
-      <View>
-        <HomeScreen/>
-      </View>
-      // </Provider>
+        <RootNavigator/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('App', () => App);
--- dss/src/components/Login.js
+++ dss/src/components/Login.js
@@ -18,14 +18,14 @@ export default class Login extends Component {
   }
 
   static navigationOptions = {
-    title: 'Login',
+     header: null
   };
-    rend
+  
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View>
-          <Text>Hii</Text>
+          <Text>Welcome To APPS</Text>
         <Button onPress={() => navigate('Main')}
           title="Get Started">
         </Button>
@@ -33,5 +33,23 @@ export default class Login extends Component {
     )
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
 AppRegistry.registerComponent('Login', () => Login);
