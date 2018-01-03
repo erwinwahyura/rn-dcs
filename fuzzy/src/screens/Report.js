fd6eeba9f152887a420fa93f482abece57edd7e1
erwinwahyura <erwinwahyuramadhan@gmail.com>
2018-01-03 17:00:03 +0700

    search data all report odne

--- fuzzy/src/screens/Report.js
+++ fuzzy/src/screens/Report.js
@@ -19,6 +19,8 @@ export default class Report extends Component {
             week: '',
             isloading: true,
             dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}),
+            flag: false,
+            flagText: false,
         }
     }
     
@@ -27,19 +29,21 @@ export default class Report extends Component {
     };
 
     getData(obj) {
+        this.setState({flagText: false, })
         console.log('objectnya  ',obj);
         let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         if (obj === '') {
             axios.get('https://erwar.id/nilais/api/')
             .then((response) => {
                 console.log('hai ', response.data);
-                this.setState({dataSource: ds.cloneWithRows(response.data)})
+                this.setState({dataSource: ds.cloneWithRows(response.data), flagText: true, })
             })
             .catch((err) => {
                 console.log('err',err);
                 alert('Uh Oh Sorry Error!');
             })
         } else if (obj.toLowerCase().substr(0,4) === 'week') {
+            console.log('masuk ke pencarian week');
             axios.post('https://erwar.id/nilais/api/week', {
                 week: obj
             })
@@ -79,12 +83,18 @@ export default class Report extends Component {
                     onPress={() => {this.getData(this.state.week)}}
                     title="Cari!"
                 />
-                <ListView
+                {
+                    this.state.flagText === false ?
+                    null :
+                    <Text style={styles.welcome}> Data Result </Text>
+                }
+                <ListView style={{marginTop: 0}}
                     dataSource={this.state.dataSource}
                     enableEmptySections={true}
                     renderRow={(rowData) => 
                         <View>
                             <View style={styles.data}> 
+                            
                                 <Text>
                                     Id: {rowData.id}
                                 </Text>
@@ -98,11 +108,11 @@ export default class Report extends Component {
                                 </Text>
 
                                 <Text>
-                                    Nilai: {rowData.nilai}
+                                    Nilai: {rowData.nilai.toString().substr(0,4)}
                                 </Text>
 
                                 <Text>
-                                    Week: {rowData.week}
+                                    Week: {rowData.tag}
                                 </Text>
 
                                 <Text>
@@ -118,20 +128,86 @@ export default class Report extends Component {
     }
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+      marginBottom: 20,
+    },
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+    },
+    
+    data: {
+      flex: 1,
+      backgroundColor: '#f1f8ff',
+      padding: 15,
+      marginBottom: 5,
+      marginTop: 5,
+      borderTopWidth: 1,
+      borderRightWidth: 1,
+      borderBottomWidth: 1,
+      borderLeftWidth: 1,
+      borderColor: '#eaeaea',
+      borderRadius: 5,
+    },
+    button: {
+      flexDirection: 'row',
+      backgroundColor: '#DDDDDD',
+      padding: 1,
+      marginTop: 5,
+      borderRadius: 5,
+      marginBottom: 3,
+      alignItems: 'center',
+    },
+    frameEdit: {
+      padding: 20,
+    },
+    inFrameEdit: {
+      backgroundColor: '#f1f8ff',
+      padding: 10,
+      borderTopWidth: 1,
+      borderRightWidth: 1,
+      borderBottomWidth: 1,
+      borderLeftWidth: 1,
+      borderColor: '#eaeaea',
+      borderRadius: 5,
+    },
+    inFrameEditButton: {
+      flex: 1,
+      alignItems: 'flex-start',
+      alignSelf: 'auto',
+      
+   },
+    buttonEDIT: {
+      flexDirection: 'row',
+      backgroundColor: '#eaeaea',
+      justifyContent: 'flex-start',
+      padding: 5,
+      margin: 15,
+      borderRadius: 5,
+    },
+    buttonAddKaryawan: {
+      alignItems: 'center',
+      backgroundColor: '#DDDDDD',
+      borderRadius: 5,
+      marginRight: 80,
+      marginLeft: 80,
+      marginBottom: 10,
+    },
+    buttonList: {
+      flex: 1,
+      alignItems: 'flex-start',
+      flexDirection: 'row',
+      backgroundColor: '#eaeaea',
+      borderRadius: 5,
+    }
+  });
